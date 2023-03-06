c5ac435a55ace08b2c8357dcbee81a1d60e17239
Yu Chen <yu.chen@live.ie>
2023-03-06 11:45:42 +0800

    feat try_dequeue

--- smake.js
+++ smake.js
@@ -1,8 +1,10 @@
-const { LLVM } = require('smake');
+const { LLVM } = require('@smake/llvm');
+const { vscode } = require('@smake/llvm-vscode');
 const { LibQueue } = require('./lib');
 
 const test = new LLVM('test', 'arm64-apple-darwin');
 test.files = ['src/test.cc'];
 LibQueue.config(test);
+vscode(test);
 
 module.exports = [test];
--- src/test.cc
+++ src/test.cc
@@ -7,6 +7,7 @@
 using namespace std::chrono_literals;
 
 ex::safe_queue<std::string> q;
+ex::safe_queue<std::string> q2;
 
 int main() {
   std::thread t1([]() {
@@ -23,7 +24,18 @@ int main() {
     }
   });
 
+  std::thread t3([]() {
+    while (true) {
+      auto msg = q2.try_dequeue(1s);
+      if (msg.has_value())
+        std::cout << *msg << std::endl;
+      else
+        std::cout << "Timeout" << std::endl;
+    }
+  });
+
   t1.join();
   t2.join();
+  t3.join();
   return 0;
 }
